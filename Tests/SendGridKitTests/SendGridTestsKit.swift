5b6a119a9456fd1b47b394cde36c1d8e00e2ed97
Francesco Paolo Severino <francescopaolo.severino@gmail.com>
2024-09-18 19:13:31 +0200

    Improve testing and DocC

--- Tests/SendGridKitTests/SendGridTestsKit.swift
+++ Tests/SendGridKitTests/SendGridTestsKit.swift
@@ -27,15 +27,51 @@ struct SendGridKitTests {
 
         let emailContent = EmailContent("This email was sent using SendGridKit!")
 
+        let setting = Setting(enable: true)
+        let mailSettings = MailSettings(
+            bypassListManagement: setting,
+            bypassSpamManagement: setting,
+            bypassBounceManagement: setting,
+            footer: Footer(enable: true, text: "footer", html: "<strong>footer</strong>"),
+            sandboxMode: setting
+        )
+
+        let trackingSettings = TrackingSettings(
+            clickTracking: ClickTracking(enable: true, enableText: true),
+            openTracking: OpenTracking(enable: true, substitutionTag: "open_tracking"),
+            subscriptionTracking: SubscriptionTracking(
+                enable: true,
+                text: "sub_text",
+                html: "<strong>sub_html</strong>",
+                substitutionTag: "sub_tag"
+            ),
+            ganalytics: GoogleAnalytics(
+                enable: true,
+                utmSource: "utm_source",
+                utmMedium: "utm_medium",
+                utmTerm: "utm_term",
+                utmContent: "utm_content",
+                utmCampaign: "utm_campaign"
+            )
+        )
+
+        let asm = AdvancedSuppressionManager(groupID: 21, groupsToDisplay: ["group1", "group2"])
+
         let email = SendGridEmail(
             personalizations: [personalization],
             from: fromEmailAddress,
             content: [emailContent],
-            attachments: [attachment]
+            attachments: [attachment],
+            asm: asm,
+            mailSettings: mailSettings,
+            trackingSettings: trackingSettings
         )
         
-        await withKnownIssue {
+        try await withKnownIssue {
             try await client.send(email: email)
+        } when: {
+            // TODO: Replace with `false` when you have a valid API key
+            true
         }
     }
 }
