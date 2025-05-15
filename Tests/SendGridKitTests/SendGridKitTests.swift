6a6c0a53b9d7809a3308b82c7011923e1daaff74
Stevenson Michel <130018170+thoven87@users.noreply.github.com>
2025-05-14 21:38:12 -0400

    Update Tests/SendGridKitTests/SendGridKitTests.swift
    Co-authored-by: Francesco Paolo Severino <96546612+fpseverino@users.noreply.github.com>

--- Tests/SendGridKitTests/SendGridKitTests.swift
+++ Tests/SendGridKitTests/SendGridKitTests.swift
@@ -133,8 +133,8 @@ struct SendGridKitTests {
     func requestUploadURL() async throws {
         try await withKnownIssue {
             let response = try await emailValidationClient.getBulkValidationUploadURL(fileType: .csv)
-            #expect(response.uploadUri != "")
-            #expect(response.jobId != "")
+            #expect(!response.uploadUri.isEmpty)
+            #expect(!response.jobId.isEmpty)
         } when: {
             credentialsAreInvalid
         }
